bf6ef72b70c683c20ede30ff907a8262ff9adbaa
Lokesh Gopi <Lokesh.Gopi@expeditors.com>
2024-07-19 11:23:56 -0400

    Final Draft

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { EventEmitter } from '@angular/core';
 // import { Coffee } from '../interface/coffee';
 // import { Coffee } from '../../data/coffee'
-
 
 import { ApiService } from '../services/api.service';
 import { NgForOf } from '@angular/common';
@@ -11,6 +11,7 @@ import { HttpClient } from '@angular/common/http';
 import { PopupComponent} from '../popup/popup.component';
 import { CoffeeHttpService } from '../coffee-http.service';
 import { Coffee } from '../../data/coffee-data';
+import { AppComponent } from '../app.component';
 
 // import { RouterModule, Routes } from '@angular/router';
 // import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -26,34 +27,35 @@ import { Coffee } from '../../data/coffee-data';
 		PopupComponent,
 	],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 
 export class ListComponent implements OnInit {
 
-	stage: Coffee []=[];
-	coffee: Coffee []=[];
-
-	selectedOption : string = '';
-	count: any;
-	delFlag : boolean = true;
-
-	sortingoptions: string[] = ['Brand (A-Z)', 'Brand (Z-A)'];
+	coffee					: Coffee []=[];
+	coffeeCount			: any;
+	selectedOption 	: string = '';
+	toDeleteID			: any = 0;
+	confirmDelete 	: boolean = false;
+	deleteMessage		: boolean = false;
+	sortingOptions	: string[] = ['Brand (A-Z)', 'Brand (Z-A)'];
 
 	constructor(private apiService:ApiService, private http: HttpClient, private httpService: CoffeeHttpService) { }
+
 	ngOnInit(): void {
 		this.apiService.coffee().subscribe(response => {
 			this.coffee = response;
-			// this.coffee  = this.coffee.filter(o => o.active);
-			this.count = this.coffee.length;
-			// this.coffee = this.coffee.sort((a, b) => a.id> b.id? 1 : -1);
+			this.coffee  = this.coffee.filter(o => o.active);
+			this.coffeeCount = this.coffee.length;
 		});
+		this.toDeleteID = 0;
+		this.confirmDelete = false;
 	}
 
+	//Sort brand name based on user selection
 	onSortChange() {
 		let colname = '';
 		let sortype = '';
-
 		if (this.selectedOption == 'Brand (A-Z)') {
 			colname = 'roaster';
 			sortype = 'asc'
@@ -67,29 +69,42 @@ export class ListComponent implements OnInit {
 		});
 	}
 
-	clickView(id : any) {
+	//Capture object ID when user clicks Delete button
+	itemToDelete(cid : any) {
+		this.toDeleteID = cid;
+		console.log(this.toDeleteID)
 	}
 
-	clickEdit(id : any) {
+	//Get user confirmation through pop up modal
+	getConfirmation(deleteFlag : boolean) {
+		this.confirmDelete = deleteFlag;
+		console.log(this.confirmDelete)
+		if (this.confirmDelete && this.toDeleteID != 0) {
+			this.deleteCoffee();
+			this.confirmDelete = false;
+			this.toDeleteID = 0;
+		}
 	}
 
-	clickDelete(cid : any) {
-			console.log(cid);
-			const todelete = this.coffee.filter(obj => obj.id == cid)[0];
-			todelete.active = false;
-			console.log(todelete);
-			this.httpService.updateCoffee(todelete).subscribe(response => {
-				console.log(response.status);
-				// setTimeout(() => window.location.reload(), 1000);
-			});
-
+	//Delete coffee item from list if confirmDelete flag is 'true'; set 'Active' = false
+	deleteCoffee () {
+		const toDelete = this.coffee.filter( obj => obj.id == this.toDeleteID)[0];
+		toDelete.active = false;
+		this.httpService.updateCoffee(toDelete).subscribe(response => {
+			console.log(response.status);
+		});
+		setTimeout(() => window.location.reload(), 1000);
 	}
 
-	getdeleteflag(deleteFlag : boolean) {
-		this.delFlag = deleteFlag;
+	clickView(id : any) {}
+	clickEdit(id : any) {}
+
+
+
+	test () {
+		this.deleteMessage    = true;
+		setTimeout(() => (this.deleteMessage = false),2000);
 	}
-
-
 }
 
 
@@ -111,3 +126,6 @@ export class ListComponent implements OnInit {
 // 	singleOrigin: todelete[0].singleOrigin,
 // 	tastingNotes: todelete[0].tastingNotes,
 // }
+
+
+// sort - this.coffee = this.coffee.sort((a, b) => a.id> b.id? 1 : -1);
